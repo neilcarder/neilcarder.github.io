e35014ed78d2b9fbce4cb1002eb3b8e68c7f001e
Neil Carder <ncarder.design@gmail.com>
2020-11-19 12:38:31 +0000

    improved lazy load script

--- assets/js/photo-content.js
+++ assets/js/photo-content.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
         beforeSend: function(xhr, status) {
             // TODO: show spinner
             $('#spinner').show();
+            //setTimeout(($('#spinner').hide()), 4000);
         },
         complete: function() {
             // TODO: hide spinner
@@ -27,45 +28,70 @@ $(document).ready(function() {
     });
     
     // *****My version of a Lazy load script*****
-    var snippets = ['two', 'three', 'four', 'five', 'six'], //array for snippet files
-    counter = -1; //starts the counter at "two" becasue of counter + 1 
+       
+    var snippets = ['two', 'three', 'four', 'five', 'six'], //array for html snippet files
+    counter = -1; //starts the counter at "two" becasue of (counter + 1) in loadAction
 
-    $(document).scroll(function() {    
-        //var scrollToBottom = $(window).scrollTop() == ($(document).height() - $(window).height()); // when scroll gets to bottom of page
-        var scrollToBottom =($(window).scrollTop() + $(window).height() >= $(document).height());
-        
-        
-        //Lazy Load for Stills Section
+    function getDocHeight() {
+        var D = document;
+        return Math.max(
+            D.body.scrollHeight, D.documentElement.scrollHeight,
+            D.body.offsetHeight, D.documentElement.offsetHeight,
+            D.body.clientHeight, D.documentElement.clientHeight
+        );
+    }
+
+    //Lazy Load Function
+    function loadAction() {
+        var scrollToBottom =($(window).scrollTop() + $(window).height() >= (getDocHeight() - 500));
+
         if ((scrollToBottom) && $("*").hasClass('stills')) { //when scroll reaches bottom and has the right class of .stills
             if(counter == 4){
-                // If max html-files reached, Do Nothing
+                // If max html-files reached, Stop counting
                 counter = counter
             } else {
-                counter = (counter + 1) // increment the counter - Add % snippets.length; to reset the counter to 0  
+                $(window).off("scroll"); // Performs the action only once. Important for TJ Gallery
+                counter = (counter + 1); // increment the counter - Add % snippets.length; to reset the counter to 0  
                 snippetNumber = snippets[counter]; // changes the array item for each count
                 $.get("snippets/stills-" + snippetNumber + ".html", function(data){ //fecthes next Snippet in Line
                     $("#content").append(data); //adds data into the photo content section
                     $("#content").waitForImages(function() { //applies Image layout after all images have been loaded
                         $('.stills.' + snippetNumber).tjGallery();
+                        // The action below resets the scroll action on the lazy load
+                        $(window).scroll(function() {    
+                            loadAction(); 
+                            //console.log(toggle);
+                        });
                     });
                     gallery.refresh(); //destroys and rebuilds LightBox Gallery
-            }); 
-        } 
+                });             
+            } 
         //Lazy Load for Montage Section
         } else if ((scrollToBottom) && $("*").hasClass('montage-flex')) { //when scroll reaches bottom and has the right class of montage-flex
             if(counter == 3){
-                // If max html-files reached, Do Nothing
+                // If max html-files reached, Stop Counting
                 counter = counter
             } else {
-                counter = (counter + 1) // increment the counter - Add % snippets.length; to reset the counter to 0  
-                snippetNumber = snippets[counter]; // 
-                //console.log(snippets[counter]); // checks the new incremented value
+                $(window).off("scroll");
+                counter = (counter + 1); // increment the counter - Add % snippets.length; to reset the counter to 0  
+                snippetNumber = snippets[counter]; // changes the array item for each count
                 $.get("snippets/joiners-" + snippetNumber + ".html", function(data){ //fecthes next Snippet in Line
-                    $(".montage-flex").append(data); //adds data into the photo content section     
+                    $(".montage-flex").append(data); //adds data into the photo content section
+                    // The action below resets the scroll action on the lazy load
+                    $(window).scroll(function() {    
+                        loadAction();
+                    });    
                 }); 
-            } 
-        }
+            }
+        } 
+    }
+
+    // First call of the Lazy Load Function above
+    $(window).scroll(function() {    
+        loadAction();
     });
+
+    // *****End Lazy load script*****
 
     //Loading Stills Content on Click script - Only Loads the first Snippet
     $('.still-content').on('click', function() {
@@ -98,5 +124,4 @@ $(document).ready(function() {
         return false;
       });
 
-      
 });
